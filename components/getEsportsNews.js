b8eca27de7e52cf3186ab84319fb392e14d8f541
Sihyun Kim <k4sud0n@gmail.com>
2021-09-15 23:33:33 +0900

    Finish components

---
+++ components/getEsportsNews.js
@@ -0,0 +1,44 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const getHTML = async () => {
+    try {
+        return await axios.get('https://campnews21.com/');
+    } catch (errror) {
+        console.error(error);
+    }
+};
+
+const getEsportsNews = async () => {
+    const list = [];
+
+    const result = await getHTML().then((html) => {
+        const $ = cheerio.load(html.data);
+
+        const titleSelector = $(
+            '#modal-esports > div > div > div.modal-body.text-center > strong'
+        );
+        const contentSelector = $(
+            '#modal-esports > div > div > div.modal-body.text-center'
+        )
+            .contents()
+            .filter(function () {
+                return this.nodeType === 3;
+            });
+
+        const contentEach = contentSelector.each((i, element) => {
+            $(element).text();
+        });
+
+        titleSelector.each((i, element) => {
+            list[i] = {
+                title: $(element).text(),
+                content: contentEach[i].data,
+            };
+        });
+    });
+
+    return list;
+};
+
+module.exports = getEsportsNews;
--- index.js
+++ index.js
@@ -1,43 +1,13 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
+const getWorldSoccerNews = require('./components/getWorldSoccerNews');
+const getEsportsNews = require('./components/getEsportsNews');
 
-const getHTML = async () => {
-    try {
-        return await axios.get('https://campnews21.com/');
-    } catch (errror) {
-        console.error(error);
-    }
-};
+const worldSoccerNews = getWorldSoccerNews();
+const esportNews = getEsportsNews();
 
-const getWorldSoccerNews = () => {
-    getHTML().then((html) => {
-        const $ = cheerio.load(html.data);
+worldSoccerNews.then(function (result) {
+    console.log(result);
+});
 
-        const worldSoccerNewsList = [];
-        const worldSoccerNewsListTitleSelector = $(
-            '#modal-worldsoccer > div > div > div.modal-body.text-center > strong'
-        );
-        const worldSoccerNewsContentSelector = $(
-            '#modal-worldsoccer > div > div > div.modal-body.text-center'
-        )
-            .contents()
-            .filter(function () {
-                return this.nodeType === 3;
-            });
-
-        const worldSoccerNewsContentEach = worldSoccerNewsContentSelector.each((i, element) => {
-            $(element).text();
-        });
-
-        worldSoccerNewsListTitleSelector.each((i, element) => {
-            worldSoccerNewsList[i] = {
-                title: $(element).text(),
-                content: worldSoccerNewsContentEach[i].data,
-            };
-        });
-
-        console.log(worldSoccerNewsList);
-    });
-};
-
-getWorldSoccerNews();
+esportNews.then(function (result) {
+    console.log(result);
+});
